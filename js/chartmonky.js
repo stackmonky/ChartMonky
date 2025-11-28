2c322bb231bd753fa0d2419b7bf808319574eea6
Brendon <brendonhampton1988@gmail.com>
2025-11-28 13:57:57 -0800

    fixed issue with browser not seeing ChartMonky variable

--- js/chartmonky.js
+++ js/chartmonky.js
@@ -4,6 +4,9 @@ window.ChartMonky = {
     charts: {}
 };
 
+const ChartMonky = window.ChartMonky;
+
+
 // Create an SVG element
 ChartMonky.utils.createSVG = function(width, height) {
     const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
